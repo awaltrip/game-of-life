c152ba90908b80a1e101ac78b95db815c88374d8
Anna Waltrip <annawaltrip@gmail.com>
2017-11-20 00:32:43 -0500

    Added functionality for diagonal row of 3

--- src/main/java/com/annawaltrip/GameOfLife.java
+++ src/main/java/com/annawaltrip/GameOfLife.java
@@ -4,8 +4,15 @@ public class GameOfLife {
 	
 	public static int[][] life(int[][] field) {
 		int[][] nextGen = { { 0, 0, 0 },
-				 			{ 0, 0, 0 },
-				 			{ 0, 0, 0 } };
+				{ 0, 0, 0 },
+				{ 0, 0, 0 } };
+		
+		boolean diagonal1 = field[0][2] == 1 && field[1][1] == 1 && field[2][0] == 1;
+		boolean diagonal2 = field[0][0] == 1 && field[1][1] == 1 && field[2][2] == 1;
+		
+		if (diagonal1 || diagonal2)
+			nextGen[1][1] = 1;
+		
 		return nextGen;
 	} 
 
--- src/test/java/com/annawaltrip/GameOfLifeTest.java
+++ src/test/java/com/annawaltrip/GameOfLifeTest.java
@@ -8,12 +8,12 @@ public class GameOfLifeTest {
 	@Test
 	public void one_cell_alone_dies() {
 		int[][] field = { { 0, 0, 0 },
-						  { 0, 1, 0 },
-						  { 0, 0, 0 } };
+				{ 0, 1, 0 },
+				{ 0, 0, 0 } };
 		
 		int[][] expected = { { 0, 0, 0 },
-							 { 0, 0, 0 },
-							 { 0, 0, 0 } };
+				{ 0, 0, 0 },
+				{ 0, 0, 0 } };
 		
 		Assert.assertArrayEquals(expected, GameOfLife.life(field));
 	}
@@ -21,19 +21,49 @@ public class GameOfLifeTest {
 	@Test
 	public void two_cells_alone_die() {
 		int[][] field1 = { { 0, 0, 0 },
-				  		   { 1, 1, 0 },
-				  		   { 0, 0, 0 } };
+				{ 1, 1, 0 },
+				{ 0, 0, 0 } };
 		
 		int[][] field2 = { { 1, 0, 0 },
-		  		  		   { 0, 1, 0 },
-		  		  		   { 0, 0, 0 } };
+				{ 0, 1, 0 },
+				{ 0, 0, 0 } };
 		
 		int[][] expected = { { 0, 0, 0 },
-					 		 { 0, 0, 0 },
-					 		 { 0, 0, 0 } };
+				{ 0, 0, 0 },
+				{ 0, 0, 0 } };
 		
 		Assert.assertArrayEquals(expected, GameOfLife.life(field1));
 		Assert.assertArrayEquals(expected, GameOfLife.life(field2));
 	}
-
+	
+	@Test
+	public void four_lone_cells_die() {
+		int[][] field = { { 1, 0, 1 },
+				{ 0, 0, 0 },
+				{ 1, 0, 1 } };
+		
+		int[][] expected = { { 0, 0, 0 },
+				{ 0, 0, 0 },
+				{ 0, 0, 0 } };
+		
+		Assert.assertArrayEquals(expected, GameOfLife.life(field));
+	}
+	
+	@Test
+	public void three_diagonal_cells_become_one() {
+		int[][] field1 = { { 1, 0, 0 },
+				{ 0, 1, 0 },
+				{ 0, 0, 1 } };
+		
+		int[][] field2 = { { 0, 0, 1 },
+				{ 0, 1, 0 },
+				{ 1, 0, 0 } };
+		
+		int[][] expected = { { 0, 0, 0 },
+				{ 0, 1, 0 },
+				{ 0, 0, 0 } };
+		
+		Assert.assertArrayEquals(expected, GameOfLife.life(field1));
+		Assert.assertArrayEquals(expected, GameOfLife.life(field2));
+	}
 }
