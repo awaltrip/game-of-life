33b988d4c20abf05cd107fc3f85a327c8bfc6c26
Anna Waltrip <annawaltrip@gmail.com>
2017-11-20 19:39:48 -0500

    Added functionality for the block pattern

--- src/main/java/com/annawaltrip/GameOfLife.java
+++ src/main/java/com/annawaltrip/GameOfLife.java
@@ -9,9 +9,14 @@ public class GameOfLife {
 		
 		boolean diagonal1 = field[0][2] == 1 && field[1][1] == 1 && field[2][0] == 1;
 		boolean diagonal2 = field[0][0] == 1 && field[1][1] == 1 && field[2][2] == 1;
-		
 		if (diagonal1 || diagonal2)
 			nextGen[1][1] = 1;
+		
+		boolean block1 = field[0][0] == 1 && field[0][1] == 1 && field[1][0] == 1 && field[1][1] == 1;
+		boolean block2 = field[1][1] == 1 && field[1][2] == 1 && field[2][1] == 1 && field[2][2] == 1;
+		if (block1 || block2) {
+			nextGen = field;
+		}
 		
 		return nextGen;
 	} 
--- src/test/java/com/annawaltrip/GameOfLifeTest.java
+++ src/test/java/com/annawaltrip/GameOfLifeTest.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 public class GameOfLifeTest {
 	
 	@Test
-	public void one_cell_alone_dies() {
+	public void oneCellAloneDies() {
 		int[][] field = { { 0, 0, 0 },
 				{ 0, 1, 0 },
 				{ 0, 0, 0 } };
@@ -19,7 +19,7 @@ public class GameOfLifeTest {
 	}
 	
 	@Test
-	public void two_cells_alone_die() {
+	public void twoCellsAloneDie() {
 		int[][] field1 = { { 0, 0, 0 },
 				{ 1, 1, 0 },
 				{ 0, 0, 0 } };
@@ -37,7 +37,7 @@ public class GameOfLifeTest {
 	}
 	
 	@Test
-	public void four_lone_cells_die() {
+	public void fourLoneCellsDie() {
 		int[][] field = { { 1, 0, 1 },
 				{ 0, 0, 0 },
 				{ 1, 0, 1 } };
@@ -50,7 +50,7 @@ public class GameOfLifeTest {
 	}
 	
 	@Test
-	public void three_diagonal_cells_become_one() {
+	public void threeDiagonalCellsBecomeOne() {
 		int[][] field1 = { { 1, 0, 0 },
 				{ 0, 1, 0 },
 				{ 0, 0, 1 } };
@@ -65,5 +65,22 @@ public class GameOfLifeTest {
 		
 		Assert.assertArrayEquals(expected, GameOfLife.life(field1));
 		Assert.assertArrayEquals(expected, GameOfLife.life(field2));
+	}
+	
+	@Test
+	public void blockPatternRemainsTheSame() {
+		int[][] field1 = { { 1, 1, 0 },
+				{ 1, 1, 0 },
+				{ 0, 0, 0 } };
+		
+		int[][] field2 = { { 0, 0, 0 },
+				{ 0, 1, 1 },
+				{ 0, 1, 1 } };
+		
+		int[][] expected1 = field1;
+		int[][] expected2 = field2;
+		
+		Assert.assertArrayEquals(expected1, GameOfLife.life(field1));
+		Assert.assertArrayEquals(expected2, GameOfLife.life(field2));
 	}
 }
